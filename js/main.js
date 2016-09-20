ee6f078293ed377663ccc3eeb7efc9dcc780ede6
lezmit <lezmit.gs@hotmail.com>
2016-09-20 17:14:14 -0500

    version 2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 window.addEventListener("load",function(){
 	var boton = document.getElementById("twittear");
+	var anclaTexto = document.getElementById("msg");
+	var contadorTexto=document.getElementById("contador-caracteres");
+	var caracter=contadorTexto.innerHTML=140;
+		
 	boton.addEventListener("click",function(e){
 		e.preventDefault();
-		var anclaTexto = document.getElementById("msg").value;
+		textTwuiter=anclaTexto.value;
 		var nuevoAncla = document.createElement("div");
-		nuevoAncla.innerHTML=anclaTexto;
+		nuevoAncla.innerHTML=textTwuiter;
 		var resultado= document.getElementById("resultado");
 		resultado.insertBefore(nuevoAncla,resultado.childNodes[0]);
+		boton.disabled=true;
+		anclaTexto.value="";
 	});
-	
+
+	anclaTexto.addEventListener("keyup",function(){
+		boton.disabled=false;  
+		var longitudCaracteres = anclaTexto.value.length;
+		if(longitudCaracteres<=caracter){
+			contadorTexto.innerHTML=caracter-longitudCaracteres;
+		}
+		else{
+			contadorTexto.innerHTML=caracter-longitudCaracteres;
+		}
+	});
+		
 });
